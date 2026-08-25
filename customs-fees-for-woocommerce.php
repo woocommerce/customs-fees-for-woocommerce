74cad9f26a5837d0ef795cd308c5c49ba69e06c7
bartech <bartech@bartech.dev>
2026-08-25 10:51:24 +0200

    [CUSFEES-31] Tweak - WooCommerce 11.1 Compatibility.

--- customs-fees-for-woocommerce.php
+++ customs-fees-for-woocommerce.php
@@ -14,8 +14,8 @@
  * Requires at least: 7.0
  * Tested up to:      7.1
  * Requires PHP:      7.4
- * WC requires at least: 10.8
- * WC tested up to:   11.0
+ * WC requires at least: 10.9
+ * WC tested up to:   11.1
  * Woo: 18734005702334:78cd9350327fbb9496bc3fbfd7335b53
  *
  * @package CustomsFeesForWooCommerce
